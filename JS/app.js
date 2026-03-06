87afa0fdbdf3bb58eec812bd714a035c197b6e5d
sunoos <m.sunoos@gmail.com>
2026-03-06 18:57:56 +0400

    Practice

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,14 @@
-console.log("........test")
+const name='sunoos';
+const age='30';
+const city='Dubai'
+
+const details=`My name is ${name} from ${city} and ${age} years old`;
+
+const num=50;
+let str=num.toString()
+let oneTwo='123'
+let strnum=Number(oneTwo)
+isStudent=true
+
+let fruit=['apple','orrange','banana','starw','mango']
+console.log(fruit   )

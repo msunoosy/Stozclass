328655bf2d62304ee571e5600d78497752268eb7
sunoos <m.sunoos@gmail.com>
2026-03-26 00:17:00 +0400

    task is working

---
+++ JS/ex1/app.js
@@ -0,0 +1,111 @@
+
+// Define a UI Vars
+
+const form = document.querySelector("#task-form");
+const taskInput = document.querySelector("#task");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#search");
+
+
+// Load all event listerners
+
+function loadEventListerners(){
+
+    // Add task event
+    form.addEventListener("submit", addTask);
+
+    // Clear task lists
+    clearBtn.addEventListener("click", clearTask);
+
+    taskList.addEventListener("click",removeTask)
+
+       filter.addEventListener("keyup", search);
+    
+
+}
+
+
+
+loadEventListerners();
+
+
+function addTask(e){
+    e.preventDefault();
+
+
+    if(taskInput.value === ""){
+        alert("Please fill the form");
+    }else{
+        
+        // Create element
+        const li = document.createElement("li");
+
+        // Add class
+        li.className = "collection-item";
+
+        // Add a inner text
+        li.innerText = taskInput.value;
+
+        // Create a new link element
+        const link = document.createElement("a");
+
+        // Add class to link
+        link.className = "delete-item secondary-content";
+
+        // Add a icon into link
+        link.innerHTML = `<i class="fa fa-remove"></i>`;
+
+        // Add a link to li
+        li.appendChild(link);
+
+        // Add a li to ul
+        taskList.appendChild(li);
+
+    }
+}
+
+
+function clearTask(){
+
+    // taskList.innerHTML = "";
+
+     const listItems = Array.from(taskList.children);
+
+     console.log(listItems);
+
+     listItems.forEach(function(li, index, arr){
+        li.remove();
+     })
+
+}
+
+function removeTask(e){
+e.target.parentElement.parentElement.remove()
+}
+
+function search(e){
+
+  const text = e.target.value.toLowerCase();
+ 
+    document.querySelectorAll(".collection-item").forEach(function(task){
+        
+        const item = task.innerText;
+
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = "block";
+        }else{
+            task.style.display = "none";
+        }
+
+    })
+
+}
+
+
+
+
+
+
+
+

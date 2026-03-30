c483cb7e89b24fdfc9f94326565ffcf8092ea4f2
sunoos <m.sunoos@gmail.com>
2026-03-30 23:23:26 +0400

    Generate random HEX color

--- Color Pallet/app.js
+++ Color Pallet/app.js
@@ -1,2 +1,11 @@
 const palette = document.getElementById("palette");
 const button = document.getElementById("generateBtn");
+
+let boxes = [];
+
+// Generate random HEX color
+function randomColor() {
+  return "#" + Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0");
+}

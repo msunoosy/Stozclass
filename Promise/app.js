d613002eee468c0dd23595e93e7fef525e730d79
sunoos <m.sunoos@gmail.com>
2026-04-16 21:46:09 +0400

    add user

--- Promise/app.js
+++ Promise/app.js
@@ -1,14 +1,22 @@
-console.log("hi")
+
 
 function getuser() {
   fetch('https://69e114e8b1cb62b9f3168d86.mockapi.io/api/v1/user').then(res=>res.json())
   .then(users=>{
-    users.forEach(user=>{
-        console.log("ID :",user.id)
-        console.log("ID :",user.name)
-        console.log(".....")
-    })
+    console.table(users)
   })
 }
 
-getuser();
+
+
+function postuser(){
+fetch('https://69e114e8b1cb62b9f3168d86.mockapi.io/api/v1/user', {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ id: 6, email: "sunoos" })
+})
+.then(res => res.json())
+.then(console.log);
+}
+
+postuser()
